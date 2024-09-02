200334ec8838f498e5fbf07660d57fc41ae304fe
lupiego <matheus.lufiego@gmail.com>
2024-09-02 03:31:47 -0300

    a

---
+++ script.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('lottie-animation'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'img/lula.json',
+});
